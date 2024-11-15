9c3a732e141c2b361760b828060a39b2734a28f1
sjlee80 <sjlee80@gmail.com>
2024-11-16 07:56:47 +0900

    Fix: depoly base, polygon

--- contract/hardhat.config.js
+++ contract/hardhat.config.js
@@ -7,8 +7,23 @@ module.exports = {
   networks: {
     sepolia: {
       url: process.env.SEPOLIA_RPC,
-      accounts: [process.env.PRIVATE_KEY],
+      accounts: [process.env.SEVICE_KEY],
       chainId: 11155111,
+    },
+    polygon_pos: {
+      url: process.env.POLYGON_RPC,
+      accounts: [process.env.SEVICE_KEY],
+      chainId: 137,
+    },
+    base_mainnet: {
+      url: process.env.BASE_RPC,
+      accounts: [process.env.SEVICE_KEY],
+      chainId: 8453,
+    },
+    neon_devnet: {
+      url: process.env.NEON_DEVNET_RPC,
+      accounts: [process.env.SEVICE_KEY],
+      chainId: 245022926,
     },
   },
 };
